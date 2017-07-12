3c22e085ad02768a76bb0e116eb4cf5a23e3e045
sxy <sxyengene@163.com>
2017-07-12 23:50:06 +0800

    node modules

--- storyCrawler.js
+++ storyCrawler.js
@@ -2,11 +2,12 @@ const cheerio = require('cheerio');
 const http = require('http');
 const fs = require('fs');
 
-var host = 'http://m.wukongks.com';
-var path = '/book/read?bkid=56983105&crid=';
-var crid = '3644001';
+var host = 'http://www.xxbiquge.com';
+var path = '/1_1378/4001123.html';
+// var crid = '3644001';
 
-var url = host+path+crid;
+// var url = host+path+crid;
+var url = host+path;
 catchPage(url);
 
 // var index = 1;
@@ -30,26 +31,32 @@ function catchPage(url){
 
 function handleHtml($){
 
-    var title = $('.title').text();
-    var text = $('.text').html();
+    var title = $('.bookname h1').text();
+    var text = $('#content').html();
     var content = '';
 
-    text = text.replace(/\<p\>/g,'');
-    text = text.replace(/\<\/p\>/g,'\n');
+    text = text.replace(/&nbsp;/g,'');
+    text = text.replace(/<br>/g,'\n');
     content = title + '\n' + text;
 
 
-    fs.writeFile('b.txt',content,{
+    fs.writeFile('九鼎记.txt',content,{
         flag:'a'
     });
 
-    if(crid == 3966326){
-        return;
-    }
+    // if(crid == 3966326){
+        // return;
+    // }
     
     var html = $.html();
-    crid = html.match(/crid=(.*)\&/)[1];
-    url = host+path+crid;
+    path = $('.bottem2 a').eq(2).attr('href');
+    if(typeof path == 'undefined'){
+        return;
+    }
+    // path = html.match(/\<a href=\"(.*)\"\>下一章\<\/a\>/)[1];
+    console.log(path);
+    // url = host+path+crid;
+    url = host+path;
     catchPage(url);
 }
 
